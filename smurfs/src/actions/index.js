21e225d2d51876c778b8e83cd7c3cfd66a2bc8ef
Mandi Haase <haase1020@gmail.com>
2020-02-21 13:19:23 -0500

    update index file, then reducers and actions files

---
+++ smurfs/src/actions/index.js
@@ -0,0 +1,38 @@
+import axios from 'axios';
+
+//fetch smurf from server
+export const FETCH_SMURF_START = 'FETCH_SMURF_START';
+export const FETCH_SMURF_SUCCESS = 'FETCH_SMURF_SUCCESS';
+export const FETCH_SMURF_FAILURE = 'FETCH_SMURF_FAILURE';
+
+//add smurf to server
+export const ADD_SMURF_START = "ADD_SMURF_START";
+export const ADD_SMURF_SUCCESS = 'ADD_SMURF_SUCCESS';
+export const ADD_SMURF_FAILURE = ' ADD_SMURF_FAILURE';
+
+//async action creator
+
+export const getSmurfs = () => dispatch => {
+    dispatch ({ type: FETCH_SMURF_START });
+    axios
+        .get('http://local:host:3333/smurfs')
+        .then(res => {
+            console.log(res.data);
+            dispatch({ type: FETCH_SMURF_SUCCESS, payload: res.data });
+        })
+        .catch(error => {
+            dispatch({ type: FETCH_SMURF_FAILURE, payload: error.response });
+        });
+};
+
+export const addSmurfs = (addSmurfData) => dispatch => {
+    dispatch({ type: ADD_SMURF_START });
+    axios
+        .post('http://localhost:3333/smurfs', addSmurfData)
+        .then(res => {
+            dispatch({ type: ADD_SMURF_SUCCESS, payload: res.data });
+        })
+        .catch(error => {
+            dispatch({ type: ADD_SMURF_FAILURE, payload: error.response })
+        })
+};
--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -7,7 +7,7 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
-// import reducer from './reducers';
+import reducer from './reducers';
 
 
 const composeEnhancers = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ || compose;
